ac18b9e56ca680fe35cfda631fde0c92be7b10e0
Mario Resl <mario@resl.info>
2024-04-28 19:30:51 +0200

    refactor

--- src/components/ui/track-card.tsx
+++ src/components/ui/track-card.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { ITrack } from "../../tracks.ts";
+import lando from "../../assets/404.jpg";
 
 interface Props {
   track: ITrack;
 }
 
 export function TrackCard({ track }: Props) {
-  if(!track) return (
-    <div className="flex items-center justify-center h-[500px] min-h-[500px]">Loading...</div>
-  );
-
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-[450px] min-h-[450px] flex flex-col justify-center">
-      <div>
-        <img className="rounded-t-lg" style={{width: 1000}} src={track.imageUrl} alt=""/>
-      </div>
-      <div className="p-5">
-        <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 text-center">
-          {track.name}
-        </h5>
-        <div className="">
-          <img src={track.flagUrl} alt={`Flag of ${track.country}`} className="w-10 h-auto mb-2"/>
+    <div className="w-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-[500px] min-h-[500px] flex flex-col justify-center items-center">
+      {track ? (
+        <>
+          <img className="rounded-t-lg w-full object-cover" src={track.imageUrl} alt=""/>
+          <div className="p-5 flex flex-col items-center">
+            <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 text-center drop-shadow-xl">
+              {track.name}
+            </h5>
+            <img src={track.flagUrl} alt={`Flag of ${track.country}`} className="w-16 h-auto mb-2 drop-shadow-2xl"/>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
+              {track.country}
+            </p>
+          </div>
+        </>
+      ) : (
+        <div>
+          <img src={lando} alt="Lando Norris" className="w-56 h-auto"/>
         </div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-          {track.country}
-        </p>
-      </div>
+      )}
     </div>
   );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -19,10 +19,16 @@ export function App() {
           baseHue={550}
           className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
         >
-          <h1 className="text-4xl font-bold text-center text-white">Formula 1 2023 - Track</h1>
-          <p className="text-white">Jeder kennt es wenn man sich für keine Strecke entscheiden kann dann ist das toold das beste für dich.</p>
-          <TrackCard className="m-4" track={track}/>
-          <button className="p-[3px] relative" onClick={handleClick}>
+          <h1 className="text-6xl font-bold text-center text-white mb-5">Formula 1 2023 - RandInt</h1>
+          <p className="text-gray-300 font-thin text-xl">
+            Kennst du das Problem, dich nicht für eine Rennstrecke entscheiden zu können? Unser Tool nimmt dir diese
+            Entscheidung ab und wählt automatisch eine spannende Strecke für dich aus. Perfekt für jeden Formel-1-Fan!
+          </p>
+          <p className="text-gray-300 mb-10 font-bold text-xl">
+            Drücke einfach auf den Button unten, um eine Strecke auszuwählen.
+          </p>
+          <TrackCard track={track}/>
+          <button className="p-[3px] relative mt-10" onClick={handleClick}>
             <div className="absolute inset-0 bg-gradient-to-r from-gray-200 to-purple-500 rounded-lg"/>
             <div
               className="px-8 py-2  bg-gray-100 rounded-[6px]  relative group transition duration-200 text-dark hover:bg-transparent">
